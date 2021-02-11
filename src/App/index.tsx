08b8d95eb743492b3a6a330998926784b138fd89
Andrey <a.harkovets@gmail.com>
2021-02-11 18:57:27 +0200

    Initial commit.

---
+++ src/App/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component, ReactElement } from 'react';
+
+import { SafeAreaView, Text, View } from 'react-native';
+
+
+
+class App extends Component {
+  private _delthis = null;
+
+
+  render(): ReactElement {
+    this._delthis;
+
+    return <SafeAreaView>
+      <Text>
+        APP
+      </Text>
+    </SafeAreaView>;
+  }
+}
+
+
+
+export default App;
